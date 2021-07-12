dfc9a29f9c0aaebc832100945f230f835cdd9d8b
Nikhil Ravindra Gurav <67793936+Nikks143@users.noreply.github.com>
2021-07-12 20:13:19 +0530

    Add files via upload

---
+++ Que_1.java
@@ -0,0 +1,12 @@
+package Home.Work;
+//1) Write a program to create a new String object with the contents of a character array
+public class Que_1 {
+
+	public static void main(String[] args) 
+	{
+		char c[] = {'n','i','k','h','i','l'};
+		String s = new String(c);
+		System.out.println(s);
+	}
+
+}
